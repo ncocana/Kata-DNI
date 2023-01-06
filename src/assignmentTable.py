33113bde36a0373501e84907f51186e51c4f978e
ncocana <ncocana@cifpfbmoll.eu>
2023-01-06 16:56:29 +0100

    feat(assignmentTable.py): create class for assignment table

---
+++ src/assignmentTable.py
@@ -0,0 +1,26 @@
+class AssignmentTable:
+
+    def __init__(self):
+        
+        self.table = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
+    
+    def getLenghtTable(self):
+
+        return len(self.table)
+    
+    def getPositionLetter(self, position):
+
+        try:
+            return self.table[position]
+        except:
+            return "This letter is not in use."
+
+    def getLetter(self, numbersDni):
+
+        positionLetter = int(numbersDni) % self.getLenghtTable()
+        return self.getPositionLetter(positionLetter)
+
+    def showTable(self):
+
+        print(self.table)
+
